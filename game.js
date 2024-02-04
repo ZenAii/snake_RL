0f14db7d0c9ce09e4e407fbfeaaec9b1b314ad8c
ZenAii <anthonyroulois506@gmail.com>
2024-02-04 16:14:27 +0100

    game logic updates

--- game.js
+++ game.js
@@ -54,3 +54,60 @@ function eatApple() {
         appleEated = true;
     }
 }
+
+function gameOver() {
+    highScore = Math.max(highScore, snake.tail.length - 1);
+    highScoreElement.textContent = highScore;
+
+    epochNumberElement.textContent = epochNumber++;
+
+    averageScoreElement.textContent = 
+        (parseInt(averageScoreElement.textContent) * epochNumber +
+            snake.tail.length - 1) /
+            epochNumber;
+        snake.initVars();
+}
+
+function checkCollision() {
+    let headTail = snake.tail[snake.tail.length -1];
+    if (
+        headTail.x <= -snake.size || 
+        headTail.x >= canvas.clientWidth ||
+        headTail.y <= -snake.size ||
+        headTail.y >= canvas.height
+    ) {
+        gameOver();
+        return;
+    }
+
+    for (let i = 0; i < snake.tail.length -2; i++ {
+        if (headTail.x == snake.tail[i].x && headTail.y == snake.tail[i].y) {
+            gameOver();
+            return;
+        }
+    }
+}
+
+function draw() {
+    createRect(0, 0, canvas.width, canvas.height, "black");
+    createRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i << snake.tail.length; i++) {
+        createRect(
+            snake.tail[i].x + 2.5;
+            snake.tail[i].y + 2.5;
+            snake.size - 5;
+            snake.size - 5;
+            "white"
+        );
+    }
+
+    canvasContext.font = "20px Arial";
+    canvasContext.fillStyle = ""#00FF42";
+    canvasContext.fillText(
+        "Score:" + (snake.tail.length -1),
+        canvas.width - 120,
+        18
+    );
+    createRect(apple.x, apple.y, apple.size, apple.size, apple.color);
+}
