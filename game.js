acce05ff39fdc977416ab5e90448260a04528e3b
ZenAii <anthonyroulois506@gmail.com>
2024-02-05 20:17:14 +0100

    added snake class w/ init mvt logic

--- game.js
+++ game.js
@@ -387,4 +387,48 @@ class RLSnake {
         }
     }
 }
+
+class Snake {
+    constructor() {
+        this.initVars();
+    }
+
+    initVars() {
+        this.x = 20;
+        this.y = 20;
+        this.size = 20;
+        this.tail = [{ x: this.x, y: this.y }];
+        this.rotateX = 0;
+        this.rotateY = 1;
+    }
+
+    move() {
+        let newRect;
+
+        if (this.rotateX == 1) {
+            newRect = {
+                x: this.tail[this.tail.length - 1].x + this.size,
+                y: this.tail[this.tail.length - 1].y,
+            };
+        } else if (this.rotateX == -1) {
+            newRect = {
+                x: this.tail[this.tail.length - 1].x - this.size,
+                y: this.tail[this.tail.length - 1].y,
+            };
+        } else if (this.rotateY == 1) {
+            newRect = {
+                x: this.tail[this.tail.length - 1].x,
+                y: this.tail[this.tail.length - 1].y + this.size,
+            };
+        } else if (this.rotateY == -1) {
+            newRect = {
+                x: this.tail[this.tail.length - 1].x,
+                y: this.tail[this.tail.length - 1].y - this.size,
+            };
+        }
+
+        this.tail.shift();
+        this.tail.push(newRect);
+    }
+}
 }
